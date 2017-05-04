26dc8c8c6ef7ce204464aa817ec65809b2aecd95
Edilene <edilene_cruz@yahoo.com.br>
2017-05-03 23:23:57 -0600

    implements class to identify player

--- source/ticTacToe.js
+++ source/ticTacToe.js
@@ -2,10 +2,23 @@ $(document).ready(function(){
   var turn = "X"
   $('td').click(function(event){
     $(event.target).text(turn);
+    $(event.target).addClass(turn);
       if (turn == "X") {
         turn = "O"
       } else {
         turn = "X"
       }
+      //check board for winning combination
+  var board = [" ", " ", " ", " ", " ", " ", " "];
+
+
+      // if(checkWin()) {
+      //   alert(turn + 'wins!')
+      // }
+      //
+      // function checkWin(){
+      //
+      // }
+  // })
   })
-});
+})
